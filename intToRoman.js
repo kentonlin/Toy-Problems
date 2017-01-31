d74cbaf8630dcf9a5e364191c890e43d157bc00f
Kenton <kentonlin1@gmail.com>
2017-01-31 03:21:35 -0500

    inToRoman

---
+++ intToRoman.js
@@ -0,0 +1,26 @@
+var intToRoman = function(num) {
+  var romans = {
+    "M": 1000,
+    "CM": 900,
+    "D": 500,
+    "CD": 400,
+    "C": 100,
+    "XC": 90,
+    "L": 50,
+    "XL": 40,
+    "X": 10,
+    "IX": 9,
+    "V": 5,
+    "IV": 4,
+    "I": 1
+  };
+  if(num === 0){
+    return "";
+  }
+  for(var key in romans){
+    if(num >= romans[key]){
+      return key + intToRoman(num - romans[key]);
+    }
+  }
+};
+intToRoman(14)
